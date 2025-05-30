4bae0938635190bac1a8182879baaaf7a45589c2
Gaming-minzza <gamingsathsaraaponsu9900@gmail.com>
2025-05-30 21:33:56 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -4,6 +4,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "𝐋ᴀᴋᴀ-𝐌ᴅ=oox0XAqZ#f2VO-DGAFij0cB8xLUHwLcI1oCPtmOtyKcwEjDKXBzc",
+SESSION_ID: process.env.SESSION_ID || "𝐋ᴀᴋᴀ-𝐌ᴅ=k9BhWB6I#IJHayoX_Mm_qqJffnsJ2DL1d1J8enBmg-Vj1HQbYXTk",
 POSTGRESQL_URL: process.env.POSTGRESQL_URL === undefined ? 'postgresql://postgres:lakamd@db.ndduyrkebubwlbwiljee.supabase.co:5432/postgres' : process.env.POSTGRESQL_URL
 };
